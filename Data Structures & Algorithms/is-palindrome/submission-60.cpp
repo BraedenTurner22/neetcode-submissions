e84972afe1f37aac4930b1b8d6e7f67a2f8c9f42
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-06-28 20:06:27 -0700

    Add: is-palindrome - submission-60

---
+++ Data Structures & Algorithms/is-palindrome/submission-60.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size()-1;
+
+        while (l <= r) {
+            while(!std::isalnum(s[l]) && l <= r) l++;
+            while(!std::isalnum(s[r]) && l <= r) r--;
+
+            if (std::tolower(s[l]) != std::tolower(s[r]) && l <= r) return false;
+
+            l++;
+            r--;
+        } 
+        return true;
+    }
+};
