76013d6ae73458efd26eb5f1de5b5a4e74c3ad1d
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-07-06 22:00:37 -0700

    Add: evaluate-reverse-polish-notation - submission-19

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-19.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<int> s;
+
+        for (auto& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int digitTwo = s.top(); s.pop();
+                int digitOne = s.top(); s.pop();
+                if (token == "+") s.push(digitOne + digitTwo);
+                else if (token == "-") s.push(digitOne - digitTwo);
+                else if (token == "*") s.push(digitOne * digitTwo);
+                else if (token == "/") s.push(digitOne / digitTwo);
+            }
+            else {
+                s.push(std::stoi(token));
+            }
+        }
+        return s.top();
+    }
+};
