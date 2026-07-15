eb204b920fbb862f4f338ecaa9a3f5d5d8564865
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-07-14 22:11:08 -0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.cpp
@@ -0,0 +1,61 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        for (int i = 0; i < 26; ++i) {
+            children[i] = nullptr;
+        }
+        isEnd = false;
+    }
+};
+
+class PrefixTree {
+public:
+    TrieNode* root;
+
+    PrefixTree() {
+        root = new TrieNode();    
+    }
+    
+    void insert(string word) {
+
+        TrieNode* currNode = root;
+
+        for (const char& c : word) {
+            int i = c - 'a';
+            if (currNode->children[i] == nullptr) currNode->children[i] = new TrieNode();
+            currNode = currNode->children[i];
+
+            if (&c == &word.back()) currNode->isEnd = true;
+        }
+        
+    }
+    
+    bool search(string word) {
+        
+        TrieNode* currNode = root;
+
+        for (const char& c : word) {
+            int i = c - 'a';
+            if (currNode->children[i] == nullptr) return false;
+            currNode = currNode->children[i];
+
+            if (&c == &word.back()) return currNode->isEnd;
+        }
+    }
+    
+    bool startsWith(string prefix) {
+
+        TrieNode* currNode = root;
+
+        for (const char& c : prefix) {
+            int i = c - 'a';
+            if (currNode->children[i] == nullptr) return false;
+            currNode = currNode->children[i];
+        }
+
+        return true;
+    }
+};
