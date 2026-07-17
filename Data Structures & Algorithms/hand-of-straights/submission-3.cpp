c8ad63596e2b53f1247c158937dac4b7405e6198
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-07-16 23:14:05 -0700

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+
+        if (hand.size() % groupSize != 0) return false;
+
+        std::unordered_map<int, int> count;
+        for (const int& h : hand) count[h]++;
+
+        for (const int& h : hand) {
+            int start = h;
+            while (count.contains(start-1)) start--;
+            while (start <= h) {
+                while (count[start] > 0) {
+                    for (int i = start; i < start+groupSize; ++i) {
+                        if (count[i] == 0) return false;
+                        count[i]--;
+                    }
+                }
+                start++;
+            }
+        }
+        return true;
+    }
+};
