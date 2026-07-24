587fc4f4e41362e681317138a5c917bd5b251421
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-07-24 10:20:29 -0700

    Add: insert-new-interval - submission-5

---
+++ Data Structures & Algorithms/insert-new-interval/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+       std::vector<std::vector<int>> result;
+
+       for (int i{0}; i < intervals.size(); ++i) {
+        int& start = newInterval[0];
+        int& end = newInterval[1];
+        if (intervals[i][1] < start) {
+            result.push_back(intervals[i]);
+        }
+        else if (intervals[i][0] > newInterval[1]) {
+            result.push_back(newInterval);
+            copy(intervals.begin()+i, intervals.end(), back_inserter(result));
+            return result;
+        }
+        else {
+            start = std::min(start, intervals[i][0]);
+            end = std::max(end, intervals[i][1]);
+
+        }
+       }
+       result.push_back(newInterval);
+       return result;
+    }
+};
