e35b0f9b7d38462ab41606d32b6e394ffd3734a3
Braeden Turner <107643244+BraedenTurner22@users.noreply.github.com>
2026-08-04 13:39:17 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){
+            return a.start < b.start;
+        });
+
+
+        for (int i{1}; i < intervals.size(); ++i) {
+            Interval prevInterval = intervals[i-1];
+            Interval currInterval = intervals[i];
+            if (prevInterval.end > currInterval.start) return false;
+        }
+
+        return true;
+    }
+};
